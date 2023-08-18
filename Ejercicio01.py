d42a4e2d764340227252eab50c9153ed04b64e8e
rcdelvalle <rcdelvalle2017@gmail.com>
2023-08-18 11:23:20 -0300

    agregue comentarios

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,3 +1,5 @@
+#punto 1
+#suma de dos numeros
 numero1=7
 numero2=11
 suma=numero1+numero2
@@ -5,6 +7,8 @@ suma=numero1+numero2
 print(suma)
 
 
+#punto 2
+#dos cadenas de texto que se concatenan
 cadena1="Hola a todos"
 cadena2=" mi nombre es Carlos"
 mensaje=cadena1+cadena2
@@ -12,6 +16,8 @@ mensaje=cadena1+cadena2
 print(mensaje)
 
 
+#punto 3
+#solicita que el usuario ingrese nombre y dni
 nombre=input("Ingrese su nombre: ")
 dni=input("Ingrese su DNI: ")
 
