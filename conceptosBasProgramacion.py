9b67fc7b3b49a5a74bdf7c20b4ea830dd074159d
rcdelvalle <rcdelvalle2017@gmail.com>
2023-08-23 16:37:34 -0300

    Ejercicio 2

--- conceptosBasProgramacion.py
+++ conceptosBasProgramacion.py
@@ -5,3 +5,23 @@ while ((numImp % 2) == 0):
     numImp = (int(input("Ingrese un nro impar ")))
 
 print ("El numero ingresado es impar")
+
+
+#Ejercicio 2.
+print("Ingrese dos nros")
+num1 = (float(input("Ingrese el primer nro ")))
+num2 = (float(input("Ingrese el segundo nro " )))
+print("Tiene 3 opciones (1. sumar, 2. restar y 3. multiplicar)")
+opcion = (int(input("Ingrese una opcion ")))
+
+while (opcion<1 or opcion>3):
+    opcion = (int(input("Ingrese una opcion ")))    
+
+if opcion==1:
+    resultado = num1 + num2
+elif opcion==2:
+    resultado = num1 - num2
+elif opcion==3:
+    resultado = num1 * num2
+
+print(f"El resultado es {resultado}")
