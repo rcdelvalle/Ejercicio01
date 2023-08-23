cdb446e5a91c33532c9f1f6c83bfdd0d1469eaa4
rcdelvalle <rcdelvalle2017@gmail.com>
2023-08-23 09:45:00 -0300

    Ejercicio 1

---
+++ conceptosBasProgramacion.py
@@ -0,0 +1,7 @@
+# Ejercicio 1.
+numImp = (int(input("Ingrese un nro impar ")))
+
+while ((numImp % 2) == 0):
+    numImp = (int(input("Ingrese un nro impar ")))
+
+print ("El numero ingresado es impar")
